c0ab43719f6b442597629868c979279878dfd05d
ellencheck <ellencheck16@gmail.com>
2025-07-20 04:19:10 -0400

    Update place.js

--- scripts/place.js
+++ scripts/place.js
@@ -1,17 +1,19 @@
-document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
 
 const temp = parseFloat(document.getElementById("temp").textContent);
 const wind = parseFloat(document.getElementById("wind").textContent);
 
-function calculateWindChill(t, w) {
+function calculateWindChill(temp, wind) {
   return (
-    13.12 + 0.6215 * t - 11.37 * Math.pow(w, 0.16) + 0.3965 * t * Math.pow(w, 0.16)
+    13.12 +
+    0.6215 * temp -
+    11.37 * Math.pow(wind, 0.16) +
+    0.3965 * temp * Math.pow(wind, 0.16)
   ).toFixed(1);
 }
 
 if (temp <= 10 && wind > 4.8) {
-  document.getElementById("chill").textContent = calculateWindChill(temp, wind) + " °C";
+  document.getElementById("chill").textContent = calculateWindChill(temp, wind);
 } else {
   document.getElementById("chill").textContent = "N/A";
 }
