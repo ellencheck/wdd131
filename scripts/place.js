a8b548689ccecb755f833a195c5aea97195d78b1
ellencheck <ellencheck16@gmail.com>
2025-07-20 03:23:52 -0400

    Update place.js

--- scripts/place.js
+++ scripts/place.js
@@ -1,19 +1,22 @@
-document.getElementById("lastModified").textContent = document.lastModified;
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("lastModified").textContent = document.lastModified;
 
-const temp = parseFloat(document.getElementById("temp").textContent);
-const wind = parseFloat(document.getElementById("wind").textContent);
+  const temp = parseFloat(document.getElementById("temp").textContent);
+  const wind = parseFloat(document.getElementById("wind").textContent);
 
-function calculateWindChill(temp, wind) {
-  return (
-    13.12 +
-    0.6215 * temp -
-    11.37 * Math.pow(wind, 0.16) +
-    0.3965 * temp * Math.pow(wind, 0.16)
-  ).toFixed(1);
-}
+  function calculateWindChill(temp, wind) {
+    return (
+      13.12 +
+      0.6215 * temp -
+      11.37 * Math.pow(wind, 0.16) +
+      0.3965 * temp * Math.pow(wind, 0.16)
+    ).toFixed(1);
+  }
 
-if (temp <= 10 && wind > 4.8) {
-  document.getElementById("chill").textContent = calculateWindChill(temp, wind);
-} else {
-  document.getElementById("chill").textContent = "N/A";
-}
+  if (temp <= 10 && wind > 4.8) {
+    document.getElementById("chill").textContent = calculateWindChill(temp, wind);
+  } else {
+    document.getElementById("chill").textContent = "N/A";
+  }
+});
+
