85cdac4d97b8448a4512ed48419a46812b88bcb9
ellencheck <ellencheck16@gmail.com>
2025-07-25 01:25:07 -0400

    Update filtered-temples.js

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -61,7 +61,7 @@ const temples = [
     dedicated: "2017, May, 21",
     area: 12000,
     imageUrl:
-      "https://www.churchofjesuschrist.org/imgs/5ec026c4efeaaa19a98e40f0f1b4c6069ae63517/full/320%2C/0/default"
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/paris-france/400x225/paris-france-temple-exterior-1554765-wallpaper.jpg"
   },
   {
     templeName: "Rome Italy",
@@ -69,7 +69,7 @@ const temples = [
     dedicated: "2019, March, 17",
     area: 13500,
     imageUrl:
-      "https://www.churchofjesuschrist.org/imgs/08d5a9e90a0c8347a61d17335775c5e118b33a9a/full/3840%2C/0/default"
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/rome-italy/400x225/rome-italy-temple-exterior-1599936-wallpaper.jpg"
   },
   {
     templeName: "Kyiv Ukraine",
@@ -77,7 +77,7 @@ const temples = [
     dedicated: "2010, August, 29",
     area: 2061,
     imageUrl:
-      "https://www.churchofjesuschrist.org/bc/content/ldsorg/church/news/2014/03/14/580-Kiev%20Ukraine%20LDS%20Temple%201129616.jpg"
+      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/kyiv-ukraine/400x250/kyiv-ukraine-temple-lds-1129616-wallpaper.jpg"
   }
 ];
 
